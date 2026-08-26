12dea6e41a9a1aaf2c6a726f94867628cc8167de
fashionstorediscount28-hash <fashionstorediscount28@gmail.com>
2026-08-26 13:59:10 +0200

    Upgrade service worker to biblioscan-v2
    Updated service worker to version 2 with improved caching strategies.

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
-const CACHE='biblioscan-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='biblioscan-v2';
+
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(
+        keys.filter(k=>k!=='biblioscan-v2').map(k=>caches.delete(k))
+      )
+    ).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    fetch(e.request).catch(()=>
+      caches.match(e.request)
+    )
+  );
+});
